4ebe1f089128e47169c969f68e94bca9f4c2ffb5
Ricardo Brown <57880227+Server101@users.noreply.github.com>
2025-07-26 23:53:41 -0400

    Update8.0

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: '3.142.144.88',            // or use your cloud DB host
   database: 'portfolio_db',     // your PostgreSQL DB name
   password: '5g4yu5i4ytud56i',
-  port: 3001,
+  port: 5432,
 });
 
 module.exports = pool;
