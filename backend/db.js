e4d266ef9042fd3acf99c21d5f5fa2b8957609d9
Ricardo Brown <57880227+Server101@users.noreply.github.com>
2025-07-26 23:48:01 -0400

    Update 7.1

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'portfolio_user',        // e.g., 'postgres'
+  user: 'portfolio_web',        // e.g., 'postgres'
   host: '3.142.144.88',            // or use your cloud DB host
-  database: 'yportfolio_db',     // your PostgreSQL DB name
+  database: 'portfolio_db',     // your PostgreSQL DB name
   password: '5g4yu5i4ytud56i',
-  port: 5432,
+  port: 3001,
 });
 
 module.exports = pool;
